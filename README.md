cc5832eb0e14f442cfe4f3007d35a51a7c47c562
Pradit Rattanapongchinda <pradit.ra@tdshop.io>
2023-06-29 16:56:29 +0700

    Update

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ Install k8s kind `brew install kind`
 
 Create cluster `kind create cluster --name k8s-local`
 
+```sh
+cat <<EOF | kind create cluster --name k8s-local --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+```
--- tutorials/006-service-ingress.sh
+++ tutorials/006-service-ingress.sh
@@ -2,6 +2,15 @@
 
 kubectl apply -f ../resources/service.yaml
 # service is kind of load balance which will route traffic into group of pod that's matched with 
-# label selector ie. app=nginx
-kubeclt describe services/nginx
-kubectl port-forward svc/nginx 8080:80
+# label selector ie. app=backend
+kubeclt describe services/backend-svc
+kubectl port-forward svc/backend-svc 8080
+
+
+# install nginx ingress-controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl get pods -n ingress-nginx
+
+# create ingress
+kubectl apply -f ../resources/ingress.yaml
+
